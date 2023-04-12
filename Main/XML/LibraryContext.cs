f771cdda0ce146202b5bb35d5423724a5ee93280
Johnathon Millard <JM2073@student.aru.ac.uk>
2023-04-12 11:28:56 +0100

    xmlContext
    Started the groundwork for using XmlContext instead of xDocument

---
+++ Main/XML/LibraryContext.cs
@@ -0,0 +1,16 @@
+﻿using Main.Models;
+
+namespace Main.XML
+{
+    public class LibraryContext : XmlContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Log> Logs { get; set; }
+
+        public LibraryContext(string dataFolder)
+            : base(dataFolder)
+        {
+        }
+    }
+}
